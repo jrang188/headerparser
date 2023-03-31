2035cb8b6ebefdba82381900a6b6de279618f4d1
Justin Ang <jrang@sfu.ca>
2023-03-30 17:59:48 -0700

    added ipaddress req

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app = express();
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.set('trust proxy', true);
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,6 +23,13 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip;
+  res.json({
+    ipaddress: ip,
+  })
 });
 
 // listen for requests :)
